40365a16da991ef70c1703263758303f72425f1d
alexsan-dev <as62971@gmail.com>
2020-04-20 22:15:51 -0600

    FINAL VERSION

--- Source/LightRoom.java
+++ Source/LightRoom.java
@@ -4,26 +4,6 @@ import javax.swing.*;
 
 public class LightRoom {
   public static void main(String[] args) {
-    // LinkedList<String> prueba = new LinkedList<String>();
-
-    // prueba.add("Hola");
-    // prueba.add("Mundo");
-    // prueba.add("Nueva");
-
-    // prueba.delete("Nueva");
-
-    // prueba.add("Ves");
-    // // // prueba.replace("Mundo", "Nuevo Mundo");
-
-    // // // // // // System.out.println(prueba.getIndex("Hasds"));
-
-    // for (int i = 0; i < prueba.getSize(); i++) {
-    // System.out.println(prueba.get(i));
-    // }
-
-    // // for (int i = -1; i >= (prueba.getSize() * -2); i--) {
-    // // System.out.println(prueba.get(i));
-    // // }
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
@@ -32,6 +12,5 @@ public class LightRoom {
     }
 
     new LoginMenu();
-
   }
 }
